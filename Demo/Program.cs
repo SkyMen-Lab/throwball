791cc5694da4a798f5bce52dbba5be41ceca09a6
German Nikolishin <skymanone@Germans-MacBook-Pro.local>
2021-09-01 16:42:20 +0100

    successfull disconnection from client

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -14,14 +14,21 @@ namespace Demo
 
         static void Main(string[] args)
         {
+            ThrowBall.Logger.Log.SetupLogger(
+                infoAction: (info) => Console.WriteLine(info),
+                warningAction: (info) => Console.WriteLine(info),
+                errorAction: (info, e) => Console.WriteLine(info)
+            );
             Console.WriteLine("Enter your choice:");
             Console.WriteLine("1. Server\n2. Client");
             choice = int.Parse(Console.ReadLine());
             switch (choice)
             {
-                case 1: CreateServer();
+                case 1:
+                    CreateServer();
                     break;
-                case 2: CreateClient();
+                case 2:
+                    CreateClient();
                     break;
                 default:
                     Console.WriteLine("Incorrect input");
@@ -35,21 +42,44 @@ namespace Demo
                 {
                     case 1:
                         ProcessUpdates(_server, 100);
-                       // _server.SendMessage(Guid.Empty, Encoding.ASCII.GetBytes(message));
+                        // _server.SendMessage(Guid.Empty, Encoding.ASCII.GetBytes(message));
                         //Console.ReadLine();
                         break;
                     case 2:
-                        ProcessUpdates(_client, 100);
-                        Console.WriteLine("Enter the message");
-                        var message = Console.ReadLine();
-                        _client.SendMessage(Encoding.ASCII.GetBytes(message));
+                        string message;
+                        if (!_client.IsConnected)
+                        {
+                            Console.WriteLine("You are not connected. connect or quit?");
+                            message = Console.ReadLine();
+                            if (message == "connect")
+                            {
+                                _client = ThrowServer.CreateClient();
+                                _client.Connect("127.0.0.1", 5000);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ending session");
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            ProcessUpdates(_client, 100);
+                            Console.WriteLine("Enter the message");
+                            message = Console.ReadLine();
+                            var s = _client.SendMessage(Encoding.ASCII.GetBytes(message));
+                            if (!s)
+                            {
+                                Console.WriteLine("You are disconnected");
+                            }
+                        }
                         break;
                     default:
                         Console.WriteLine("Incorrect input");
                         break;
                 }
             }
-            
+
         }
 
         public static void CreateClient()
@@ -59,13 +89,13 @@ namespace Demo
             _client.OnConnected = () => Console.WriteLine("Connected to server from client");
             _client.OnMessageReceived = (bytes =>
             {
-                var message= Encoding.ASCII.GetString(bytes);
+                var message = Encoding.ASCII.GetString(bytes);
                 Console.WriteLine("Message from server: " + message);
             });
             _client.OnDisconnected = () => Console.WriteLine("Disconnected from the server");
         }
-        
-        
+
+
 
         public static void CreateServer()
         {
@@ -75,7 +105,14 @@ namespace Demo
             _server.OnMessageReceived = (guid, bytes) =>
             {
                 var message = Encoding.ASCII.GetString(bytes);
-                Console.WriteLine($"Message from {guid} - {message}");
+                if (message.ToLower().Contains("disconnect"))
+                {
+                    if (_server.DisconnectClient(guid))
+                        Console.WriteLine($"Message from {guid} - {message} - trying to disconnect client");
+                    else Console.WriteLine($"Error disconneting a client {guid}");
+                }
+                else
+                    Console.WriteLine($"Message from {guid} - {message}");
             };
             _server.OnClientDisconnected = guid => Console.WriteLine($"Client disconnected - {guid}");
         }
